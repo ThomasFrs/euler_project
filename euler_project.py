6a040cb07e9c5de5766c75223b1b012bd1841cc2
Heartbreaker <66362307+ThomasFrs@users.noreply.github.com>
2022-02-17 22:34:25 +0100

    Added Problem 11

--- euler_project.py
+++ euler_project.py
@@ -1,14 +1,5 @@
 from math import *
 
-"""
-This is a javadoc style.
-
-@param param1: this is a first param
-@param param2: this is a second param
-@return: this is a description of what is returned
-@raise keyError: raises an exception
-"""
-
 class Euler():
     def __init__(self):
         print("\nwelcome to the euler project program")
@@ -45,11 +36,12 @@ class Euler():
         x = number to test if it is a prime number (divisible only by 1 and itself)
         return: True if x is a prime number, False if it isn't
         """
-        if x <= 1 or any([elt for elt in self.prime_number_list if x % elt == 0]):
-            return False
-        else:
-            self.prime_number_list.append(x)
+        if x >= 2:
+            for i in range(2, x):
+                if x % i == 0:
+                    return False
             return True
+        return False
                 
 
     def largest_prime_factor(self, number):
@@ -192,8 +184,9 @@ class Euler():
         return sum([i for i in range(max) if self.is_prime(i)])
 
     # problem 11
-    def largest_product_in_grid(self, grid, direction, length):
-        number_list = [int(i) for i in grid.split()]
+    def largest_product_in_grid(self, grid):
+        # my code which didn't work for the diagonals, so after spending a few day on it, i gave up and used lucas willems' way
+        """number_list = [int(i) for i in grid.split()]
         max = 0
         if direction == "horizontal":
             for i in range(len(number_list) - length + 1):
@@ -213,18 +206,49 @@ class Euler():
             for i in range((int(sqrt(len(number_list)))+ 1 - length)**2):
                 product = 1
                 for j in range(length):
-                    if i > sqrt(len(number_list) - length):
+                    if i > int(sqrt(len(number_list))) - length and int(sqrt(len(number_list))) % i-1 == 0:
                         i += length
-                    product *= number_list[i + (j*length+1)]
+                    product *= number_list[i + (j*(int(sqrt(len(number_list)))+1))]
                     if product > max:
                         max = product
-        return max
+        elif direction == "diagonalup" and length <= sqrt(len(number_list)):
+            for i in range((int(sqrt(len(number_list)))+ 1 - length)**2):
+                product = 1
+                for j in range(length):
+                    if i > sqrt(len(number_list) - length):
+                        i += length
+                    print(number_list[i + (j*(length-1)+length-1)])
+                    product *= number_list[i + (j*(length-1)+length-1)]
+                    if product > max:
+                        max = product
+        return max"""
+
+        number_list = [int(i) for i in grid.split()]
+        product = []
+        for i in range(400):
+            if i%20 < 17:
+                product.append(number_list[i]*number_list[i+1]*number_list[i+2]*number_list[i+3])
+            if i < 340:
+                product.append(number_list[i]*number_list[i+20]*number_list[i+40]*number_list[i+60])
+            if i%20 < 17 and i < 340:
+                product.append(number_list[i]*number_list[i+21]*number_list[i+42]*number_list[i+63])
+            if i%20 > 3 and i < 340:
+                product.append(number_list[i]*number_list[i+19]*number_list[i+38]*number_list[i+57])
+        return max(product)
+
+    # problem 12
+    def highly_divisible_triangular_number(self, divisors_number):
+        triangle_numbers_list = [1]
+        adding = 1
+        while len([i for i in range(1, triangle_numbers_list[-1]+1) if triangle_numbers_list[-1] % i == 0]) <= divisors_number:
+            adding += 1
+            triangle_numbers_list.append((triangle_numbers_list[-1]+adding))
+        return triangle_numbers_list
 
     def test_function(self):
         print("\nwelcome to the test function")
-        print(self.largest_product_in_grid("08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08 49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00 81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65 52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91 22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80 24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50 32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70 67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21 24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72 21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95 78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92 16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57 86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58 19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40 04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66 88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69 04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36 20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16 20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48", "vertical", 4))
-        print(self.largest_product_in_grid("1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16", "vertical", 4))
-        print(self.largest_product_in_grid("1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16", "diagonaldown", 4))
+        print(self.highly_divisible_triangular_number(2))
+
 
 euler = Euler()
 euler.test_function()
