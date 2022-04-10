19bf11ae3905e4adb01918ea4a072e4e2a7ed6ab
thomasfrs <thomasfrs44@gmail.com>
2022-04-10 09:11:26 +0200

    Improved 01 using comprehensions

--- 5_percent/01_multiples_of_3_or_5.py
+++ 5_percent/01_multiples_of_3_or_5.py
@@ -1,14 +1,14 @@
+import _common_functions as cf
+
+
 def sum_of_multipliers_below(max):
     """
     max: max value that contains all mutipliers of 3 and 5
     return: sum of all mutipliers of 3 and 5 below max
     """
-    multipliers = []
-    n = 0
-    while n < max:
-        if n % 3 == 0 or n % 5 == 0:
-            multipliers.append(n)
-        n += 1
-    return sum(multipliers)
+    return sum([n for n in range(1, max) if n % 3 == 0 or n % 5 == 0])
 
-assert sum_of_multipliers_below(1000) == 233168, "sum_of_multipliers_below(1000), 233168"
+
+print(cf.average_running_time(1000, sum_of_multipliers_below, 1000))
+# Average running time of sum_of_multipliers_below for 1000 tests:
+# 0.00011975049972534179 seconds
