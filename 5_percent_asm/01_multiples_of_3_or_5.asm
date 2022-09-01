06793cfe923a976e1a5fb79e06dc52f40cc925ee
thomasfrs <thomasfrs44@gmail.com>
2022-09-01 12:34:08 +0200

    Modified a bit 01 and added a module file for easier use

--- 5_percent_asm/01_multiples_of_3_or_5.asm
+++ 5_percent_asm/01_multiples_of_3_or_5.asm
@@ -1,21 +1,14 @@
 ; this might be shit but it's my first assembly program
+%include "macro.asm"
+
+section .data
+	newline db 10,0
 
 section .bss
 	sum resb 100
-	digitSpace resb 100
-	digitSpacePos resb 8
-
-section .data
-	digit db 0,10
 
 section .text
 	global _start
-
-%macro exit 0
-	mov rax, 60
-	mov rdi, 0
-	syscall
-%endmacro
 
 _start:
 	mov rax, 2
@@ -37,9 +30,10 @@ _loop:
 	je _add_sum_3 ; add rax to sum if 3/rax
 	
 	; reset rax
-	mov r8, rdx ; store rdx in another register
+	push rdx ; store rdx in another register
 	mul rbx
-	add rax, r8
+	pop rdx
+	add rax, rdx
 
 	; test if rax divisible by 5
 	mov rbx, 5
@@ -49,9 +43,10 @@ _loop:
 	je _add_sum_5 ; add rax to sum if 5/rax	
 
 	; reset rax
-	mov r8, rdx
+	push rdx
 	mul rbx
-	add rax, r8
+	pop rdx
+	add rax, rdx
 	
 	; test 
 	call _loop
@@ -77,59 +72,7 @@ _add_sum_5:
 
 _printSum:
 	mov rax, [sum]
-	call _printRAX
-
-	ret
-
-_printRAXDigit:
-	add rax, 48
-	mov [digit], al
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, digit
-	mov rdx, 2
-	syscall 
-
-	ret
-
-_printRAX:
-	mov rcx, digitSpace
-	mov rbx, 10 ; new line character
-	mov [rcx], rbx
-	inc rcx ; next character after new line
-	mov [digitSpacePos], rcx ; digitSpacePos is set to rcx
-
-_printRAXLoop:
-	mov rdx, 0 ; cancels the concatenation
-	mov rbx, 10
-	div rbx ; divide rax by 10
-	push rax ; push number/10 on top of stack
-	add rdx, 48 ; translate rdx into an ASCII number
-	
-	; increment digitSpacePos
-	mov rcx, [digitSpacePos]
-	mov [rcx], dl ; lower byte of rdx (character ASCII)
-	inc rcx
-	mov [digitSpacePos], rcx
-
-	pop rax
-	cmp rax, 0 ; tests if end of loop, quotient is 0 (last number)
-	jne _printRAXLoop
-
-; reverse the number
-_printRAXLoop2:
-	mov rcx, [digitSpacePos]
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, rcx
-	mov rdx, 1
-	syscall
-
-	mov rcx, [digitSpacePos]
-	dec rcx ; decrement rcx
-	mov [digitSpacePos], rcx
-
-	cmp rcx, digitSpace ; tests if at the beginning of number (done)
-	jge _printRAXLoop2
+	printVal rax
+	print newline
 
 	ret
